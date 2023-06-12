6b3e0bf27b19890d1e34a4ea6f01d5a4fd72993a
Developshin <shin.github@gmail.com>
2023-06-12 10:41:58 +0900

    tailwindcss 변수화 하여 사용

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,9 @@
-import dynamic from "next/dynamic";
-import { ReactNode } from "react";
+export * from '@/lib/st';
+import dynamic from 'next/dynamic';
+import { ReactNode } from 'react';
 
-const ThemeToggle = dynamic(() => import("./ThemeToggle"), {
+const ThemeToggle = dynamic(() => import('./ThemeToggle'), {
   ssr: false,
 });
+
 export { ThemeToggle };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,20 @@
-import Head from "next/head";
-import { Inter } from "next/font/google";
-import tw from "twin.macro";
+import { st } from '@/lib';
+import { Inter } from 'next/font/google';
+import tw from 'twin.macro';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
       <main className={`${inter.className}`}>
-        <div tw="bg-primary border border-gray-600 dark:bg-blue-400 m-2 p-2 ">
+        <div tw='bg-primary border border-gray-600 dark:bg-blue-400 m-2 p-2 '>
           custom tailwind primary color
         </div>
-        <div className="m-2">
-          <h1 className="px-10 bg-slate-100 text-blue-700"> heoll</h1>
-          <h2 className="px-10 bg-slate-100 text-blue-700"> heoll</h2>
-          <h3 className="px-10 bg-slate-100 text-blue-700"> heoll</h3>
+        <div className={style()}>
+          <h1 className='px-10 bg-slate-100 text-blue-700'> heoll</h1>
+          <h2 className='px-10 bg-slate-100 text-blue-700'> heoll</h2>
+          <h3 className='px-10 bg-slate-100 text-blue-700'> heoll</h3>
         </div>
       </main>
     </>
@@ -27,3 +27,5 @@ const S = tw.div`
 	p-2 m-2
 	
 `;
+
+const style = st`bg-gray-400`;
